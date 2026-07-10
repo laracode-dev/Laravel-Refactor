b78298d2974f2e20d6f2aa323fb8c515b15e2c3e
mutaznayef <dev.laracode@gmail.com>
2026-07-10 14:15:24 +0300

    Revert "feat(order): refactore store function"
    This reverts commit 63624f315d8e87ba5912e990c4ada63aa1d77fda.

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -2,17 +2,12 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Requests\StoreOrderRequest;
 use App\Models\Order;
-use App\Services\StoreOrderService;
+use App\Models\Product;
 use Illuminate\Http\Request;
 
 class OrderController extends Controller
 {
-
-    public function __construct(private StoreOrderService $storeOrderService)
-    {
-    }
 
     /**
      * Display a listing of the resource.
@@ -25,10 +20,27 @@ class OrderController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(StoreOrderRequest $request)
+    public function store(Request $request)
     {
-        $order = $this->storeOrderService->handle($request->all());
+        // validation
+        $request->validate([
+            'product_id' => 'required|numeric|exists:products,id',
+            'quantity' => 'required|numeric|min:1',
+            'address' => 'required|string'
+        ]);
+        // product
+        $product = Product::find($request->product_id);
 
+        // total price
+        $total = $product->price * $request->quntity;
+        // create order
+        $order = Order::create([
+            'user_id' => 1,
+            'product_id' => $product->id,
+            'quantity' => $request->quantity,
+            'address' => $request->address,
+            'total' => $total
+        ]);
         // return response
         return response()->json([
             'success' => true,
