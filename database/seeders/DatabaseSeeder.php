bd5da177e6bc2408f3796a70316cb6abcbaeb452
mutaznayef <dev.laracode@gmail.com>
2026-07-10 12:09:39 +0300

    Revert "feat(order):create order"
    This reverts commit 077527f198bf1f9634514a7e79b73ec46267d55f.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,8 +2,6 @@
 
 namespace Database\Seeders;
 
-use App\Models\Order;
-use App\Models\Product;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -17,17 +15,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        User::factory(10)->create();
+        // User::factory(10)->create();
 
-//        User::factory()->create([
-//            'name' => 'Test User',
-//            'email' => 'test@example.com',
-//        ]);
-
-        Product::factory(30)->create();
-        Order::factory()->create([
-            'user_id' => 1,
-            'product_id' => 1,
+        User::factory()->create([
+            'name' => 'Test User',
+            'email' => 'test@example.com',
         ]);
     }
 }
